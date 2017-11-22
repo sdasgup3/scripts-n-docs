1f3f0d53ea134255e4bb88e3a24ea8a9b9dd66af
Sandeep Dasgupta <sdasgup3@illinois.edu>
2017-11-21 19:16:03 -0600

    introducing master template for perl constructs

--- scripts/perlMasterTemplate.pl
+++ scripts/perlMasterTemplate.pl
@@ -11,9 +11,12 @@ use Cwd;
 use File::Path qw(make_path remove_tree);
 use Env;
 
+# Using comments
+=pod
 # Using some perl lib
 use lib qw( /home/sdasgup3/Github/binary-decompilation/test/utils/ );
 use utils;
+=cut
 
 # Get environment Variable
 my $home = $ENV{'HOME'};
@@ -55,18 +58,21 @@ my $sdir = "";
 if ( ( -e $file ) and ( -d $sdir ) ) {
 }
 
-# Subroutine
-sub execute {
-    my $args = shift @_;
-}
-
 # Hash / Map Usage
 my %tool_path;
 $tool_path{'llvm'} = "$home";
-my $lenhash = scalar keys %tool_path;;
+my $lenhash = scalar keys %tool_path;
 
 # Concatenate Print
-print "\n". $lenhash. "\n";
+print "\n" . $lenhash . "\n";
+
+# Keys of hash
+my @fruits = keys %tool_path;
+for my $fruit (@fruits) {
+}
+
+for my $fruit ( keys %tool_path ) {
+}
 
 # Array Usage
 my @maindirs = ( "llvm", );
@@ -77,3 +83,63 @@ push( @all, "clang" );
 # Using fileparse
 my ( $bin, $dir, $ext ) = fileparse( "a/b/c.cpp", qr/\.[^.]*/ );
 print "$dir$bin$ext" . "\n";
+
+# create Dir
+sub createDir {
+    my $args = shift @_;
+    make_path(
+        $args,
+        {
+            chmod => 0777,
+        }
+    );
+}
+
+# Useful Subroutine
+
+sub execute {
+    my $args = shift @_;
+    print "$args \n";
+    system("$args");
+}
+
+sub info {
+    my $args = shift @_;
+    system("echo  \e[4m\e[1m\e[34m$args\e[0m");
+}
+
+sub passInfo {
+    my $args = shift @_;
+    system("echo  \e[4m\e[1m\e[92m$args\e[0m");
+}
+
+sub failInfo {
+    my $args = shift @_;
+    system("echo  \e[4m\e[1m\e[91m$args\e[0m");
+}
+
+sub warnInfo {
+    my $args = shift @_;
+    system("echo  \e[4m\e[1m\e[35m$args\e[0m");
+}
+
+# Passing ref to arrays
+my @arr1 = ();
+my @arr2 = ();
+compareStates( \@arr1, \@arr2 );
+
+sub compareStates {
+    my ( $k_ref, $x_ref ) = @_;
+    my @kstates = @{$k_ref};
+    my @xstates = @{$x_ref};
+
+    # return value
+    return @xstates;
+}
+
+# Regex match
+my @xpatterns = ( qr/\$\d* = (\d+)/, qr/\$\d* = \[ ([CPAZSOIF ]*) \]/, );
+my $someline = "";
+if ( $someline =~ m/$xpatterns[0]/ ) {
+    print $1. "\n";
+}
